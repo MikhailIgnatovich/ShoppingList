b447405bba6ccc2450bdbf61d2dd3d89ea6c574c
MikhailIgnatovich <bulich198922@gmail.com>
2021-08-30 23:35:07 +0300

    Добавил MainViewModel.kt в presentation

---
+++ app/src/main/java/com/bulich/misha/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.bulich.misha.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bulich.misha.shoppinglist.data.ShopListRepositoryIMPL
+import com.bulich.misha.shoppinglist.domain.DeleteShopItemUseCase
+import com.bulich.misha.shoppinglist.domain.EditShopItemUseCase
+import com.bulich.misha.shoppinglist.domain.GetShopListUseCase
+import com.bulich.misha.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryIMPL
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
