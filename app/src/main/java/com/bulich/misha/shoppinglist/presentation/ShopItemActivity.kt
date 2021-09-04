327ec2580addb5b1ba0e6c05f64c52ad077bafb7
MikhailIgnatovich <bulich198922@gmail.com>
2021-09-04 17:37:29 +0300

    Добавил ShopItemActivity.kt и activity_shop_item.xml

---
+++ app/src/main/java/com/bulich/misha/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.bulich.misha.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bulich.misha.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
