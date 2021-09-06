bcdb429e2e82101765e5cb2d4af122e6caead9b1
MikhailIgnatovich <bulich198922@gmail.com>
2021-09-06 22:28:00 +0300

    Добавил fun newIntentAddItem и newIntentEditItem в ShopItemActivity.kt и реализовал их в MainActivity.kt

--- app/src/main/java/com/bulich/misha/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/bulich/misha/shoppinglist/presentation/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bulich.misha.shoppinglist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         setupRecycler()
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -68,7 +74,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
-
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/bulich/misha/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/bulich/misha/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,5 +1,7 @@
 package com.bulich.misha.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bulich.misha.shoppinglist.R
@@ -8,5 +10,20 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+    }
+
+    companion object {
+
+        private const val SHOP_ITEM_ID = "SHOP_ITEM_ID"
+
+        fun newIntentAddItem(context: Context): Intent {
+            return Intent(context, ShopItemActivity::class.java)
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
