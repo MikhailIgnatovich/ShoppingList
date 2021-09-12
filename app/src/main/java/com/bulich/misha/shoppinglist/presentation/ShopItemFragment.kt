2601e323a62702e2036d08ea5679710ea07cd014
MikhailIgnatovich <bulich198922@gmail.com>
2021-09-12 18:15:42 +0300

    Добавил ShopItemFragment.kt

---
+++ app/src/main/java/com/bulich/misha/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,21 @@
+package com.bulich.misha.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bulich.misha.shoppinglist.R
+
+
+class ShopItemFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+}
